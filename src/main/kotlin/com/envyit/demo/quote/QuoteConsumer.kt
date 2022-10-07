8438b77e74e3d4f693d63fa03dd45167b347383d
Michael Eder <michael.ederit@gmail.com>
2022-10-07 23:30:27 +0200

    Configure logging

--- src/main/kotlin/com/envyit/demo/quote/QuoteConsumer.kt
+++ src/main/kotlin/com/envyit/demo/quote/QuoteConsumer.kt
@@ -1,14 +1,22 @@
 package com.envyit.demo.quote
 
 import org.eclipse.microprofile.reactive.messaging.Incoming
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class QuoteConsumer {
 
+    companion object {
+        @JvmStatic
+        val logger: Logger = LoggerFactory.getLogger(QuoteConsumer::class.simpleName)
+    }
+
     @Incoming("quote-in")
     fun consume(quoteRequest: String) {
-        println(quoteRequest)
+        logger.info("Received message for UUID: $quoteRequest")
     }
 
 }
