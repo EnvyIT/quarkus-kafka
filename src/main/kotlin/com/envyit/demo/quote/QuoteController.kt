b5caacc7601c3b2af59da3da3fbaba7a07db8a03
Michael Eder <michael.ederit@gmail.com>
2022-10-07 23:02:15 +0200

    Specify completion stage

--- src/main/kotlin/com/envyit/demo/quote/QuoteController.kt
+++ src/main/kotlin/com/envyit/demo/quote/QuoteController.kt
@@ -3,6 +3,7 @@ package com.envyit.demo.quote
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
 import java.util.*
+import java.util.concurrent.CompletionStage
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -14,5 +15,5 @@ class QuoteController(@Channel("quote-out") private val quoteEmitter: Emitter<St
     @POST
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    fun publishQuoteRequest() = quoteEmitter.send(UUID.randomUUID().toString())
+    fun publishQuoteRequest(): CompletionStage<Void> = quoteEmitter.send(UUID.randomUUID().toString())
 }
